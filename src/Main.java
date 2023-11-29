83b03513abe28443f2b4fb3fd40094752ad8a321
Martin <public@e-mis.cz>
2023-11-29 13:13:54 +0100

    Oprava pozdravu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Guten morgen!");
-        System.out.println("Ahoj");
+        System.out.println("Dobrý den");
     }
 }
