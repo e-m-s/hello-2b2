de2811251aebcbbec6487b19a8a80a9100a875b7
e-m-s <public@e-mis.cz>
2023-11-29 13:14:13 +0100

    Oprava pozdravu
    Na základě požadavku zákazníka ze dne...

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Guten morgen!");
-        System.out.println("Ahoj");
+        System.out.println("Nazdar");
         System.out.println("Další výpis");
     }
 }
