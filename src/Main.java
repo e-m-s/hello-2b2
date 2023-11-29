f3511f1d96e15448a0b36b3354e9e18655cd1b2d
e-m-s <public@e-mis.cz>
2023-11-29 13:07:26 +0100

    Přidal jsem další výpis
    Na základě požadavku zákazníka ze dne...

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Guten morgen!");
         System.out.println("Ahoj");
+        System.out.println("Další výpis");
     }
 }
